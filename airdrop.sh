67a8a8361159567b6aa3375adfd62a8c4a6ed53c
Jung <jwork1994212@gmail.com>
2025-03-17 16:43:46 +0800

    feature: update blocksigner path for vm

--- airdrop.sh
+++ airdrop.sh
@@ -2,9 +2,6 @@ target_addr=$1
 
 echo "Sending native tokens to: $target_addr"
 
-cd
-cd ~/Project/liquid-functionary-configs
-
 # --- Send coins ---
 elements-cli -datadir="$PWD/01/elements" sendtoaddress $target_addr 1 "" "" true
 
