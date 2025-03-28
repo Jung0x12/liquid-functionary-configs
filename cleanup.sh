739a92da7d6650ba5cd14d4edd4d3397e4240d34
Jung <jwork1994212@gmail.com>
2025-03-28 13:42:52 +0800

    feature: implement 5 nodes

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in 01 02;do
+for i in 01 02 03 04 05;do
     elements-cli -datadir=$PWD/$i/elements/ stop
 done
 sleep 1
@@ -10,7 +10,7 @@ killall blocksigner
 
 sleep 1
 
-for i in 01 02;do
+for i in 01 02 03 04 05;do
     rm -rf $PWD/$i/elements/liquidregtest
     rm -rf $PWD/$i/blocksigner/debug.log
 done
--- runfunc.sh
+++ runfunc.sh
@@ -8,18 +8,18 @@ cli() {
 }
 
 # --- Start elementsd nodes ---
-for i in 01 02;do
+for i in 01 02 03 04 05;do
     elementsd -datadir=$PWD/$i/elements/
 done
 sleep 1
 
 # --- Start blocksigner ---
-for i in 01 02;do
+for i in 01 02 03 04 05;do
     ~/Project/liquid-functionary/target/release/blocksigner $PWD/$i/blocksigner/ >$PWD/$i/blocksigner/debug.log 2>&1 &
 done
 sleep 1
 
-# --- Create wallets and get addresses ---
+# # --- Create wallets and get addresses ---
 for i in 1 2; do
   cli $i createwallet "" false false "" false false
   sleep 1
